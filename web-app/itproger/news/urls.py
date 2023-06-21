43f960035b62aa6073342e9ff9ff59764d0066de
Nikita <nagafonov1@yandex.ru>
2023-06-21 20:04:44 +0300

    Еще урок сделали. Реализовали добавление статьи в БД. Когда ошибка вылетает блин. cannot access local variable 'error' =( А в целом отображается зашибок.

--- web-app/itproger/news/urls.py
+++ web-app/itproger/news/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.news_home, name='news_home')
+    path('', views.news_home, name='news_home'),
+    path('create', views.create, name='create')
 ]
--- web-app/itproger/news/views.py
+++ web-app/itproger/news/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Articles
+from .forms import ArticlesForm
 # Create your views here.
 
 def news_home(request):
     news = Articles.objects.order_by('-date') # Упорядочить по тайтлу. Если нужно в обратном порядке, то аргумент '-title'
     #news = Articles.objects.order_by('-date')[:2] Получим срез только две записи. [0] и [1]
     return render(request, 'news/news_home.html', {'news': news})
+
+def create(request):
+
+    if request.method == "POST":
+        form = ArticlesForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home') #После проверки формы на валидность. Метод redirect переадресовывает на home
+        else:
+            error = 'Форма была неверной'
+
+    form = ArticlesForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+
+    return render(request, 'news/create.html', data)
